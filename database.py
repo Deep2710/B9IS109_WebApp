330c779592964fd10cf25b643141808ca152b203
Deep2710 <55691334+Deep2710@users.noreply.github.com>
2020-03-03 10:30:19 +0000

    Add files via upload

---
+++ database.py
@@ -0,0 +1,32 @@
+import sqlite3
+conn = sqlite3.connect('ecommerce.db')
+
+
+conn.execute(''' create table if not exists customer (
+id integer primary key,
+firstname text,
+lastname text,
+email text,
+password text
+)''')
+
+conn.execute(''' create table if not exists categories (
+categoryId integer primary key,
+categoryName text
+)''')
+
+
+conn.execute('''create table if not exists products (
+productId integer primary key,
+productName text,
+productPrice real,
+productDescription text,
+productImage text,
+categoryId integer,
+foreign key (categoryId) references categories (categoryId)
+)''')
+
+
+
+
+conn.close()
