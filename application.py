311d57252394aec06e1f2f490124202510e87952
Deep2710 <55691334+Deep2710@users.noreply.github.com>
2020-03-10 21:28:37 +0000

    Update application.py

--- application.py
+++ application.py
@@ -171,8 +171,8 @@ def cart():
     email = session['email']
     with sqlite3.connect('ecommerce.db') as conn:
         cur = conn.cursor()
-        cur.execute("SELECT id FROM customer WHERE email = '" + email + "'")
-        id = cur.fetchone()
+        cur.execute("SELECT id, firstname FROM customer WHERE email = '" + session['email'] + "'")
+        id = cur.fetchone()[0]
         cur.execute("SELECT products.productId, products.productName, products.productPrice, products.productImage FROM products, cart WHERE products.productId = cart.productId AND cart.id = " + str(id))
         products = cur.fetchall()
         cur.execute('SELECT categoryId, categoryName FROM categories')
