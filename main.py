91a2436a060f2a0beb0f514ca2260f49621d3bfa
Deep2710 <55691334+Deep2710@users.noreply.github.com>
2020-03-13 22:03:51 +0000

    new changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import *
 import sqlite3,hashlib,os
 from werkzeug.utils import secure_filename
 import re
+import stripe
 
 
 app = Flask(__name__)
@@ -11,8 +12,12 @@ ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 stripe_keys = {
-
+  'secret_key': "sk_test_yapnauNSRccFLqmjvTRZ8tIf00ZkXVoMYS",
+  'publishable_key': "pk_test_VdvVepm6m8GIISNzD50VxWov00UPwdpIit"
 }
+
+stripe.api_key = stripe_keys['secret_key']
+
 
 @app.route("/")
 def home():
@@ -37,10 +42,11 @@ def getLoginDetails():
         else:
             loggedIn = True
             cur.execute("SELECT id,firstname FROM customer WHERE email = '" + session['email'] + "'")
-            id = cur.fetchone()[0]
-            firstname = cur.fetchone()
+            data = cur.fetchall()
+            id = data[0][0]
+            firstname = data[0][1]
             cur.execute("SELECT count(productId) FROM cart WHERE id = " + str(id))
-            totalItems = cur.fetchone()
+            totalItems = cur.fetchone()[0]
     conn.close()
     return (loggedIn,firstname,totalItems)
 
@@ -68,35 +74,37 @@ def logout():
 @app.route('/register',methods = ['GET','POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form and 'password' in request.form and 'confirmPassword' in request.form:
         firstname = request.form['firstname']
         lastname = request.form['lastname']
         email = request.form['email']
         password = request.form['password']
+        confirmPassword = request.form['confirmPassword']
 
-        #with sqlite3.connect('ecommerce.db') as conn:
-        #    cur = conn.cursor()
-        #    cur.execute("SELECT * FROM customer where email = '" + email + "'")
-        #    account = cur.fetchone()
-
-        #    if account:
-        #        msg = 'Account already exists with same email !'
-        #    elif not re.findall(r'[a-zA-Z]+',firstname ):
-        #        msg = 'Firstname must contain only characters!'
-        #    elif not re.match(r'[a-zA-Z]+',lastname ):
-        #        msg = 'Lastname must contain only characters!'
-        #    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-        #        msg = 'Invalid email address!'
-        #    elif not firstname or not password or not email or not lastname:
-        #        msg = 'Please fill out the form!'
-
-        #else:
         with sqlite3.connect('ecommerce.db') as conn:
             cur = conn.cursor()
-            cur.execute('INSERT INTO customer VALUES (NULL,?,?,?,?)',(firstname,lastname,email,hashlib.md5(password.encode()).hexdigest()))
-            conn.commit()
-            msg = 'You have successfully registered'
-            return redirect(url_for('login'))
+            cur.execute("SELECT * FROM customer where email = '" + email + "'")
+            account = cur.fetchone()
+
+
+        if not re.findall(r'[a-zA-Z]+',firstname ):
+            msg = 'Firstname must contain only characters!'
+        elif not re.match(r'[a-zA-Z]+',lastname ):
+            msg = 'Lastname must contain only characters!'
+        elif password != confirmPassword:
+            msg = 'Password does not match.'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif account:
+            msg = 'Account already exists with same email!'
+
+        else:
+            with sqlite3.connect('ecommerce.db') as conn:
+                cur = conn.cursor()
+                cur.execute('INSERT INTO customer VALUES (NULL,?,?,?,?)',(firstname,lastname,email,hashlib.md5(password.encode()).hexdigest()))
+                conn.commit()
+                msg = 'You have successfully registered'
+                return redirect(url_for('login'))
 
     elif request.method == 'POST':
         msg = 'Please fill out the form!'
@@ -160,10 +168,12 @@ def displayCategory():
             cur = conn.cursor()
             cur.execute("SELECT products.productId, products.productName, products.productPrice, products.productImage, categories.categoryName FROM products, categories WHERE products.categoryId = categories.categoryId AND categories.categoryId = " + categoryId)
             data = cur.fetchall()
+            cur.execute('SELECT categoryId, categoryName FROM categories')
+            categoryData = cur.fetchall()
         conn.close()
         categoryName = data[0][4]
         data = parse(data)
-        return render_template('displayCategory.html', data=data, loggedIn=loggedIn, firstName=firstName, categoryName=categoryName)
+        return render_template('displayCategory.html', data=data, loggedIn=loggedIn, firstName=firstName, categoryName=categoryName,categoryData=categoryData)
 
 @app.route("/addToCart")
 def addToCart():
@@ -195,7 +205,7 @@ def cart():
         cur = conn.cursor()
         cur.execute("SELECT id FROM customer WHERE email = '" + email + "'")
         id = cur.fetchone()[0]
-        cur.execute("SELECT products.productId, products.productName, products.productPrice, products.productImage FROM products, cart WHERE products.productId = cart.productId AND cart.id = " + str(id))
+        cur.execute("SELECT products.productId, products.productName, products.productPrice, products.productImage, cart.rowid FROM products, cart WHERE products.productId = cart.productId AND cart.id = " + str(id))
         products = cur.fetchall()
         cur.execute('SELECT categoryId, categoryName FROM categories')
         categoryData = cur.fetchall()
@@ -209,13 +219,13 @@ def removeFromCart():
     if 'email' not in session:
         return redirect(url_for('login'))
     email = session['email']
-    productId = int(request.args.get('productId'))
+    rowId = int(request.args.get('rowId'))
     with sqlite3.connect('ecommerce.db') as conn:
         cur = conn.cursor()
         cur.execute("SELECT id FROM customer WHERE email = '" + email + "'")
         id = cur.fetchone()[0]
         try:
-            cur.execute("DELETE FROM cart WHERE id = " + str(id) + " AND productId = " + str(productId))
+            cur.execute("DELETE FROM cart WHERE id = " + str(id) + " AND rowid = " + str(rowId))
             conn.commit()
             msg = "removed successfully"
         except:
@@ -232,6 +242,47 @@ def checkout():
     email = session['email']
     with sqlite3.connect('ecommerce.db') as conn:
         cur = conn.cursor()
+        cur.execute("SELECT id, firstname, lastname FROM customer WHERE email = '" + email + "'")
+        data = cur.fetchall()
+        id = data[0][0]
+        firstName = data[0][1]
+        lastName = data[0][2]
+        cur.execute("SELECT products.productId, products.productName, products.productPrice, products.productImage FROM products, cart WHERE products.productId = cart.productId AND cart.id = " + str(id))
+        products = cur.fetchall()
+        cur.execute('SELECT categoryId, categoryName FROM categories')
+        categoryData = cur.fetchall()
+    totalPrice = 0
+    for row in products:
+        totalPrice += row[2]
+    return render_template("checkout.html", products = products, totalPrice=totalPrice, loggedIn=loggedIn, firstName=firstName,lastName = lastName,  totalItems=totalItems,categoryData=categoryData, email = email, key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+
+    id, totalPrice = beforeCharge()
+    # amount in cents
+    amount = int(totalPrice) * 100
+
+    customer = stripe.Customer.create(
+        email=session['email'],
+        source=request.form['stripeToken']
+    )
+
+    stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='EUR',
+        description='Shopzone Charge'
+    )
+    return render_template('charge.html', amount=amount)
+
+def beforeCharge():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    loggedIn, firstName, totalItems = getLoginDetails()
+    email = session['email']
+    with sqlite3.connect('ecommerce.db') as conn:
+        cur = conn.cursor()
         cur.execute("SELECT id FROM customer WHERE email = '" + email + "'")
         id = cur.fetchone()[0]
         cur.execute("SELECT products.productId, products.productName, products.productPrice, products.productImage FROM products, cart WHERE products.productId = cart.productId AND cart.id = " + str(id))
@@ -241,7 +292,8 @@ def checkout():
     totalPrice = 0
     for row in products:
         totalPrice += row[2]
-    return render_template("checkout.html", products = products, totalPrice=totalPrice, loggedIn=loggedIn, firstName=firstName, totalItems=totalItems,categoryData=categoryData)
+
+    return(id,totalPrice)
 
 @app.route("/changePassword", methods=["GET", "POST"])
 def changePassword():
@@ -255,8 +307,9 @@ def changePassword():
         with sqlite3.connect('ecommerce.db') as conn:
             cur = conn.cursor()
             cur.execute("SELECT id, password FROM customer WHERE email = '" + session['email'] + "'")
-            id = cur.fetchone()
-            password = cur.fetchone()
+            data = cur.fetchall()
+            id = data[0][0]
+            password = data[0][1]
             if (password == oldPassword):
                 try:
                     cur.execute("UPDATE customer SET password = ? WHERE id = ?", (newPassword, id))
@@ -272,6 +325,7 @@ def changePassword():
         return render_template("changePassword.html", msg=msg)
     else:
         return render_template("changePassword.html")
+
 
 def allowed_file(filename):
     return '.' in filename and \
